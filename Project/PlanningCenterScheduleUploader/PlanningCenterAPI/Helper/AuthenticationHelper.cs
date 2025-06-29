c4a323e2c88ee83371916983b94a8b1060ba1ef6
DevlynZion <DevlynZion@users.noreply.github.com>
2025-06-29 16:17:22 +0200

    First Working Example

---
+++ Project/PlanningCenterScheduleUploader/PlanningCenterAPI/Helper/AuthenticationHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanningCenterAPI.Helper
+{
+	public static class AuthenticationHelper
+	{
+		private const string PathToSecretFile = @"D:\Projects\GitHub\PlanningCenterScheduleUploader\Document\Secret.txt";
+
+		public static string GetCredentials()
+		{
+			var lines = File.ReadAllLines(PathToSecretFile);
+
+			return $"{lines[1]}:{lines[4]}";
+		}
+	}
+}
--- Project/PlanningCenterScheduleUploader/PlanningCenterScheduleUploader/Program.cs
+++ Project/PlanningCenterScheduleUploader/PlanningCenterScheduleUploader/Program.cs
@@ -1,26 +1,77 @@
-﻿namespace PlanningCenterScheduleUploader
+﻿using System.Net.Http.Json;
+using System.Net.Http;
+using System;
+using System.Text;
+using System.Net.Security;
+using PlanningCenterAPI.Helper;
+
+namespace PlanningCenterScheduleUploader
 {
 	internal class Program
 	{
 		static void Main(string[] args)
 		{
-			Test();
+			Test2();
 			Console.ReadKey();
+		}
+
+		static async void Test2()
+		{
+			var handler = new SocketsHttpHandler()
+			{
+				PooledConnectionLifetime = TimeSpan.FromMinutes(2),
+			};
+
+			using (HttpClient client = new HttpClient(handler))
+			{
+				client.BaseAddress = new Uri("https://api.planningcenteronline.com");
+				client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", Convert.ToBase64String(Encoding.ASCII.GetBytes(AuthenticationHelper.GetCredentials())));
+				var response = await client.GetAsync("/services/v2/people");
+				response.EnsureSuccessStatusCode();
+				var responseData = await response.Content.ReadFromJsonAsync<Root>();
+				Console.WriteLine(responseData);
+			}
 		}
 
 		static async void Test()
 		{
 			var handler = new SocketsHttpHandler()
 			{
-				PooledConnectionLifetime = TimeSpan.FromMinutes(2)
+				PooledConnectionLifetime = TimeSpan.FromMinutes(2),
 			};
 
 			using (HttpClient client = new HttpClient(handler))
 			{
-				var response = await client.GetAsync("https://api.planningcenteronline.com/services/v2/people");
+				client.BaseAddress = new Uri("https://api.planningcenteronline.com");
+				client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", Convert.ToBase64String(Encoding.ASCII.GetBytes(AuthenticationHelper.GetCredentials())));
+				var response = await client.GetAsync("/people/v2/people");
 				var responseString = await response.Content.ReadAsStringAsync();
 				Console.WriteLine(responseString);
 			}
 		}
+	}
+
+	public class Root
+	{
+		public Links Links { get; set; }
+		public List<Data> Data { get; set; }
+
+		public override string ToString()
+		{
+			return $"Data={Data.Count}";
+		}
+	}
+
+
+	public class Links
+	{
+		public string Self { get; set; }
+		public string Next { get; set; }
+	}
+
+	public class Data
+	{
+		public string Type { get; set; }
+		public string Id { get; set; }
 	}
 }
