2df1dca56d45e80716a3c118feeef33cceebfe09
DevlynZion <DevlynZion@users.noreply.github.com>
2025-06-29 14:22:46 +0200

    Start Project

---
+++ Project/PlanningCenterScheduleUploader/PlanningCenterScheduleUploader/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace PlanningCenterScheduleUploader
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Test();
+			Console.ReadKey();
+		}
+
+		static async void Test()
+		{
+			var handler = new SocketsHttpHandler()
+			{
+				PooledConnectionLifetime = TimeSpan.FromMinutes(2)
+			};
+
+			using (HttpClient client = new HttpClient(handler))
+			{
+				var response = await client.GetAsync("https://api.planningcenteronline.com/services/v2/people");
+				var responseString = await response.Content.ReadAsStringAsync();
+				Console.WriteLine(responseString);
+			}
+		}
+	}
+}
